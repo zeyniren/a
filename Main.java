b9ad5d56a82051b791c310d9b6ccdd84c9a83cc4
Zeynep Iren Unal <166499215+zeyniren@users.noreply.github.com>
2024-06-26 23:04:26 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,36 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code
+
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        Student Student1 = new Student("Ivan", "Abchuhskyi", "s30827");
+        System.out.printf(Student1.toString() + "\n");
+        //StudentNo - your index number
+        Student s30410 = new Student("Artur", "Kyivskyi", "s30410");
+        System.out.printf(s30410.toString() + "\n");
+        Student s10101 = new Student("YourName", "YourSurname", "YourstudentsNumber");
+        System.out.printf(s10101.toString() + "\n");
+        Student m4a = new Student("Anastasiya", "Marozava", "S30863");
+        System.out.printf(m4a.toString() + "\n");
+        Student s30840 = new Student("Danyil", "Nikulin", "s30840");
+        System.out.printf(s30840.toString() + "\n");
+        Student s26149 = new Student("Mukhammadaziz", "Odilijonov", "s26149");
+        System.out.printf(s26149.toString() + "\n");
+        Student s30859 = new Student("Volodymyr", "Osypenko", "s30859");
+        System.out.printf(s30859.toString() + "\n");
+        Student s30324 = new Student("Leon", "Sobota", "s30324");
+        System.out.println(s30324.toString() + "\n");
+        Student s30860 = new Student("Valeria", "Suranova", "s30860");
+        System.out.println(s30860.toString() + "\n");
+        Student s28268 = new Student("Hande Melis", "Toker", "s28268");
+        System.out.println(s28268.toString() + "\n");
+        Student s30063 = new Student("Dzianis", "Tsikhanau", "s30063");
+        System.out.printf(s30063.toString() + "\n");
+        Student s29932 = new Student("Zeynep Irem", "Unal", "s29932");
+        System.out.println(s29932.toString() + "\n");
+        Student s29893 = new Student("Mikita", "Radzivonik", "s29896");
+        System.out.println(s29893.toString() + "\n");
+    }
+}
